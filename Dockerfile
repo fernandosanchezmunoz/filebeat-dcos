2d283635ee7490db58903e582041133cf3c6b8bd
Fernando Sanchez <fernandosanchezmunoz@gmail.com>
2016-11-13 20:43:45 -0500

    first commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM centos:7.2.1511
+MAINTAINER Fernando Sanchez <fernando@mesosphere.io>
+
+ADD startup.sh /startup.sh
+
+RUN curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.0.0-x86_64.rpm && \
+	sudo rpm -vi filebeat-5.0.0-x86_64.rpm 
+
+#volume 1: /var/lib/mesos to /var/lib/mesos
+#volume 2: /var/log/ to /var/log/
+
+CMD /startup.sh
---
+++ startup.sh
@@ -0,0 +1,64 @@
+sudo tee /usr/bin/dcos-journalctl-filebeat.sh<-EOF 
+#!/bin/bash
+/usr/bin/journalctl --no-tail -f \
+  -u dcos-3dt.service 				 \
+  -u dcos-3dt.socket				 \
+  -u dcos-adminrouter-reload.service \
+  -u dcos-adminrouter-reload.timer   \
+  -u dcos-adminrouter.service        \
+  -u dcos-bouncer.service            \
+  -u dcos-ca.service                 \
+  -u dcos-cfn-signal.service         \
+  -u dcos-cosmos.service             \
+  -u dcos-download.service           \
+  -u dcos-epmd.service               \
+  -u dcos-exhibitor.service          \
+  -u dcos-gen-resolvconf.service     \
+  -u dcos-gen-resolvconf.timer       \
+  -u dcos-history.service            \
+  -u dcos-link-env.service           \
+  -u dcos-logrotate-master.timer     \
+  -u dcos-marathon.service           \
+  -u dcos-mesos-dns.service          \
+  -u dcos-mesos-master.service       \
+  -u dcos-metronome.service          \
+  -u dcos-minuteman.service          \
+  -u dcos-navstar.service            \
+  -u dcos-networking_api.service     \
+  -u dcos-secrets.service            \
+  -u dcos-setup.service              \
+  -u dcos-signal.service             \
+  -u dcos-signal.timer               \
+  -u dcos-spartan-watchdog.service   \
+  -u dcos-spartan-watchdog.timer     \
+  -u dcos-spartan.service            \
+  -u dcos-vault.service              \
+  -u dcos-logrotate-master.service   \
+  -u dcos-logrotate-agent.timer            \
+  -u dcos-mesos-slave.service              \
+  -u dcos-adminrouter-agent.service        \
+  -u dcos-adminrouter-reload.service       \
+  -u dcos-adminrouter-reload.timer         \
+  -u dcos-rexray.service                   \
+  -u dcos-vol-discovery-priv-agent.service \
+  -u dcos-logrotate-agent.service          \
+  > /var/log/dcos/dcos.log 2>&1
+EOF
+
+sudo mkdir -p /var/log/dcos
+
+sudo tee /etc/filebeat/filebeat.yml <<-EOF 
+filebeat.prospectors:
+- input_type: log
+  paths:
+    - /var/lib/mesos/slave/slaves/*/frameworks/*/executors/*/runs/latest/stdout
+    - /var/lib/mesos/slave/slaves/*/frameworks/*/executors/*/runs/latest/stderr
+    - /var/log/mesos/*.log
+    - /var/log/dcos/dcos.log
+tail_files: true
+output.elasticsearch:
+  hosts: ["$ELASTIC_HOST:$ELASTIC_PORT"]
+EOF
+
+sudo chmod 0755 /usr/bin/dcos-journalctl-filebeat.sh
+sudo /usr/bin/dcos-journalctl-filebeat.sh
