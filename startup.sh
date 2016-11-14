e3909af7f60f2d883ac8f53c2b124fd824d8002f
Fernando Sanchez <fernandosanchezmunoz@gmail.com>
2016-11-14 12:21:29 -0500

    correct startup

--- startup.sh
+++ startup.sh
@@ -3,11 +3,11 @@
 if [ -z "$ELASTIC_HOST" ]; then
   ELASTIC_HOST="elastic.marathon.l4lb.thisdcos.directory"
 fi
-if [ -z "$ELASTIC_PORT"]; then
+if [ -z "$ELASTIC_PORT" ]; then
   ELASTIC_PORT=9200
 fi
 
-sed -i -e "s/ELASTIC_HOST:ELASTIC_PORT/$ELASTIC_HOST:$ELASTIC_PORT/g" /etc/filebeat.yaml
+sed -i -e "s/ELASTIC_HOST:ELASTIC_PORT/$ELASTIC_HOST:$ELASTIC_PORT/g" /etc/filebeat.yml
 
 /usr/bin/journalctl --vacuum-size=10M
 
